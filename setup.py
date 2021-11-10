b2f28b9da42016cc5fc5349ebbdd6a9dcb57942c
Andreas Stocker <AStocker@anexia-it.com>
2021-11-10 14:54:24 +0100

    SIANXKE-165: Changed package name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fh:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='python-concurrency-limit',
+    name='concurrency-limit',
     version=os.getenv('PACKAGE_VERSION', '0.0.0').replace('refs/tags/', ''),
     packages=find_packages(),
     include_package_data=True,
